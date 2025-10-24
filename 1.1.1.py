f18376f7e9b5ca2555e44b79017e73f55ad7e8c1
milana-1111 <milkyway_1_d@mail.ru>
2025-10-24 11:22:43 +0500

    урок1

---
+++ 1.1.1.py
@@ -0,0 +1,15 @@
+a = 10
+b = -3
+result1 = a % b
+print(result1)
+
+a = 15
+b = 4
+result = a // b + a % b
+print(result)
+
+x = 2
+y = 3
+z = 4
+calculation = x + y * z ** x // y
+print(calculation)
