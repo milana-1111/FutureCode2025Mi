cc936544bd8e8579874ecd61f159801144fd29fd
milana-1111 <milkyway_1_d@mail.ru>
2025-10-25 13:23:48 +0500

    урок3

---
+++ 1.1.3.py
@@ -0,0 +1,24 @@
+x = 15 / 3
+print(type (x))
+
+a = 10
+b = 5
+c = a * b
+result = c / b + a
+print(type(result))
+
+a = "12"
+b = "3.5"
+c = int(a) + float(b)
+d = str(c) + "5"
+result = float(d) * 2
+print(result)
+
+x = "10"
+y = 2
+z = 3.5
+a = int(x * y) + z
+b = str(int(z)) + x
+c = float(b) / y
+result = a + c
+print(result)
