9a3337895a3f50cfa72378d9d014125fa49d6ffb
milana-1111 <milkyway_1_d@mail.ru>
2025-10-25 12:32:13 +0500

    квадрат

---
+++ modules.py
@@ -0,0 +1,14 @@
+from time import *
+from random import randint
+from turtle import *
+start = time()
+bgcolor("yellow")
+color("red")
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+begin_fill()
