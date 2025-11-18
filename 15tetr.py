b685355ec86150c1be25247a2eabe83ae03d4972
milana-1111 <milkyway_1_d@mail.ru>
2025-11-18 12:58:43 +0500

    8урок

---
+++ 15tetr.py
@@ -0,0 +1,22 @@
+name = "Анна"
+age = 14
+print(f"Меня зовут {name}. Мне {age} лет. \n")
+
+word = "Программирование"
+print(word[0])
+print(word[-1])
+print(word[5],"\n")
+
+text = "Hello, world!"
+print(len(text))
+print(text[len(text) - 1],"\n")
+
+word = "Информатика"
+print(word[:3])
+print(word[3:7])
+print(word[-4:],"\n")
+
+word = "Кодирование"
+for char in word:
+    print(char)
+    
